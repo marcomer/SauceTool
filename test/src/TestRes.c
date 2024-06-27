a65d0d73199f2152f8dd7a175a9b5fd735e3a261
Marcus Omer <marcus.omer19@gmail.com>
2024-06-26 18:45:13 -0600

    Add testfile1 expected data structures

--- test/src/TestRes.c
+++ test/src/TestRes.c
@@ -1,5 +1,5 @@
 #include "TestRes.h"
-
+#include <string.h>
 
 const SAUCE default_record = {
   .ID = {'S','A','U','C','E'},
@@ -22,9 +22,34 @@ const SAUCE default_record = {
 
 
 SAUCE* test_get_testfile1_expected_record() {
-  return 0;
+  static SAUCE expected;
+  SAUCE_set_default(&expected);
+  memcpy(expected.Title, "TestFile1", 9);
+  memcpy(expected.Author, "marcomer", 8);
+  memcpy(expected.Date, "20240625", 8);
+  expected.FileSize = 0x18;
+  expected.DataType = 1;
+  expected.FileType = 1;
+  expected.TInfo1 = 0x0A;
+  expected.TInfo2 = 0x02;
+  expected.TInfo3 = 0;
+  expected.TInfo4 = 0;
+  expected.Comments = 2;
+  expected.TFlags = 0x02;
+  memcpy(expected.TInfoS, "IBM VGA", 7);
+  
+  return &expected;
 }
 
 SAUCE_CommentBlock* test_get_testfile1_expected_comment() {
-  return 0;
+  static SAUCE_CommentBlock block;
+  static char string[128];
+  memset(string, 0, 128);
+  memcpy(block.ID, "COMNT", 5);
+  block.lines = 2;
+  
+  memcpy(string, "This is the comments field on TestFile1. This was created using the Moebius ANSI art application.", 97);
+  block.comment = string;
+
+  return &block;
 }
