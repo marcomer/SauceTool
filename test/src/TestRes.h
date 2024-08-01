7e8a8ca8839dd74d25dd1d93f55bfe4f4d761edb
Marcus Omer <marcus.omer19@gmail.com>
2024-07-31 19:33:56 -0600

    Replace .txt extensions with .ans
    After some testing on Windows, it turns out carriage returns
    got added after cloning the repository. Forcing files to be .ans
    may prevent carriage returns.

--- test/src/TestRes.h
+++ test/src/TestRes.h
@@ -15,10 +15,10 @@
 #define SAUCE_TESTFILE3_PATH    "expect/TestFile3.ans"
 
 // No Sauce File -> File does not contain any SAUCE info
-#define SAUCE_NOSAUCE_PATH      "expect/NoSauce.txt"
+#define SAUCE_NOSAUCE_PATH      "expect/NoSauce.ans"
 
 // ShortFile -> File under 128 bytes
-#define SAUCE_SHORTFILE_PATH    "expect/ShortFile.txt"
+#define SAUCE_SHORTFILE_PATH    "expect/ShortFile.ans"
 
 // SauceButNoEOF -> TestFile1 with a missing EOF character
 #define SAUCE_SAUCEBUTNOEOF_PATH  "expect/SauceButNoEOF.ans"
@@ -27,10 +27,10 @@
 #define SAUCE_COMMENTBUTNORECORD_PATH   "expect/CommentButNoRecord.ans"
 
 // InvalidComment -> Long file with an incorrect Comments field
-#define SAUCE_INVALIDCOMMENT_PATH       "expect/InvalidComment.txt"
+#define SAUCE_INVALIDCOMMENT_PATH       "expect/InvalidComment.ans"
 
 // LongNoSauce -> A CMakeLists.txt file with no SAUCE info
-#define SAUCE_LONGNOSAUCE_PATH          "expect/LongNoSauce.txt"
+#define SAUCE_LONGNOSAUCE_PATH          "expect/LongNoSauce.ans"
 
 // OnlyRecord -> Record from TestFile2.ans but with no EOF character
 #define SAUCE_ONLYRECORD_PATH           "expect/OnlyRecord.ans"
@@ -38,68 +38,68 @@
 // NoSauceWithEOF.ans -> Contains normal file contents with an ending EOF character
 #define SAUCE_NOSAUCEWITHEOF_PATH       "expect/NoSauceWithEOF.ans"
 
-// EmptyFile.txt -> A completely empty file
-#define SAUCE_EMPTYFILE_PATH            "expect/EmptyFile.txt"
+// EmptyFile.ans -> A completely empty file
+#define SAUCE_EMPTYFILE_PATH            "expect/EmptyFile.ans"
 
 
 // Expected write file results. These files should not be changed
 
 // Result of writing to an empty file
-#define SAUCE_WRITETOEMPTY_PATH   "expect/write/WriteToEmptyExpect.txt"
+#define SAUCE_WRITETOEMPTY_PATH   "expect/write/WriteToEmptyExpect.ans"
 
 // Result of replacing TestFile1's SAUCE record
-#define SAUCE_REPLACE_PATH        "expect/write/ReplaceExpect.txt"
+#define SAUCE_REPLACE_PATH        "expect/write/ReplaceExpect.ans"
 
 // Result of appending to a file
-#define SAUCE_APPEND_PATH         "expect/write/AppendExpect.txt"
+#define SAUCE_APPEND_PATH         "expect/write/AppendExpect.ans"
 
 // Result of appending to NoSauceWithEOF.ans
-#define SAUCE_APPENDWITHEOF_PATH  "expect/write/AppendWithEOFExpect.txt"
+#define SAUCE_APPENDWITHEOF_PATH  "expect/write/AppendWithEOFExpect.ans"
 
 // File to contain the actual results of a test write
-#define SAUCE_WRITE_ACTUAL_PATH   "actual/write_actual.txt"
+#define SAUCE_WRITE_ACTUAL_PATH   "actual/write_actual.ans"
 
 
 // Expected remove file results. These files should not be changed
 
 // Result of removing a SAUCE record from TestFile3.ans
-#define SAUCE_REMOVE_ONLY_RECORD_PATH "expect/remove/RemoveOnlyRecord.txt"
+#define SAUCE_REMOVE_ONLY_RECORD_PATH "expect/remove/RemoveOnlyRecord.ans"
 
 // Result of removing a SAUCE record and CommentBlock from TestFile1.ans
-#define SAUCE_REMOVE_RECORD_AND_COMMENT_PATH  "expect/remove/RemoveBoth.txt"
+#define SAUCE_REMOVE_RECORD_AND_COMMENT_PATH  "expect/remove/RemoveBoth.ans"
 
-// Result of removing a SAUCE record from InvalidComment.txt
-#define SAUCE_REMOVE_INVALID_COMMENT_PATH     "expect/remove/RemoveInvalidComment.txt"
+// Result of removing a SAUCE record from InvalidComment.ans
+#define SAUCE_REMOVE_INVALID_COMMENT_PATH     "expect/remove/RemoveInvalidComment.ans"
 
 // File to contain the actual results of a test remove
-#define SAUCE_REMOVE_ACTUAL_PATH  "actual/remove_actual.txt"
+#define SAUCE_REMOVE_ACTUAL_PATH  "actual/remove_actual.ans"
 
 
 // Expected comment write file results. These files should not be changed.
 
 // File to contain the acutal results of a test comment write
-#define SAUCE_COMMENT_WRITE_ACTUAL_PATH     "actual/comment_write_actual.txt"
+#define SAUCE_COMMENT_WRITE_ACTUAL_PATH     "actual/comment_write_actual.ans"
 
 // AddCommentToRecord -> Contains the result of adding shortComment to TestFile2.ans
-#define SAUCE_ADDCOMMENTTORECORD_PATH       "expect/comment_write/AddCommentToRecord.txt"
+#define SAUCE_ADDCOMMENTTORECORD_PATH       "expect/comment_write/AddCommentToRecord.ans"
 
 // ReplaceExistingComment -> Contains the result of replacing TestFile1's comment with longComment
-#define SAUCE_REPLACEEXISTINGCOMMENT_PATH   "expect/comment_write/ReplaceExistingComment.txt"
+#define SAUCE_REPLACEEXISTINGCOMMENT_PATH   "expect/comment_write/ReplaceExistingComment.ans"
 
 // AddCommentAndEOFToRecord -> Contains the result of adding shortComment and an EOF to OnlyRecord.ans
-#define SAUCE_ADDCOMMENTANDEOFTORECORD_PATH   "expect/comment_write/AddCommentAndEOFToRecord.txt"
+#define SAUCE_ADDCOMMENTANDEOFTORECORD_PATH   "expect/comment_write/AddCommentAndEOFToRecord.ans"
 
 // ReplaceCommentAndAddEOF -> Contains the result of replacing SauceButNoEOF's comment with shortComment and adding EOF
-#define SAUCE_REPLACECOMMENTANDADDEOF_PATH    "expect/comment_write/ReplaceCommentAndAddEOF.txt"
+#define SAUCE_REPLACECOMMENTANDADDEOF_PATH    "expect/comment_write/ReplaceCommentAndAddEOF.ans"
 
 // SameCommentLength -> Contains the result of replacing TestFile1's comment with shortComment but padded to 2 lines
-#define SAUCE_SAMECOMMENTLENGTH_PATH          "expect/comment_write/SameCommentLength.txt"
+#define SAUCE_SAMECOMMENTLENGTH_PATH          "expect/comment_write/SameCommentLength.ans"
 
 
 // Expected comment remove file results. These files should not be changed
 
 // File to contain the actual result of a test comment remove
-#define SAUCE_COMMENT_REMOVE_ACTUAL_PATH    "actual/comment_remove_actual.txt"
+#define SAUCE_COMMENT_REMOVE_ACTUAL_PATH    "actual/comment_remove_actual.ans"
 
 // RemoveComment -> Contains the result of removing a comment from TestFile1.ans
 #define SAUCE_REMOVECOMMENT_PATH            "expect/comment_remove/RemoveComment.ans"
